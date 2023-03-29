75906aa3f44b21a9cb8ad087fab0f2268271daba
ChenYijin <chenyijin@pwrd.com>
2023-03-29 16:16:16 +0800

    useCanvas用类替换闭包，修改types

--- src/views/default-editor/hooks/useCanvas.ts
+++ src/views/default-editor/hooks/useCanvas.ts
@@ -1,104 +1,86 @@
 import { isFunction } from 'lodash-es';
-import { reactive, ref, unref } from 'vue';
-import { CanvasConfig } from './useCanvasConfig';
+import { CanvasConfig, CanvasExtendImp } from '../common/types';
 
-export type CanvasInstance = {
-  setupCanvas: (canvas: CanvasRenderingContext2D, config?: CanvasConfig) => void;
-  save: () => void;
-  redo: () => void;
-  undo: () => void;
-  clean: () => void;
-  erase(point: PointA, isLast?: boolean): void;
-  drawSmoothLine: (beginPoint: PointA, controlPoint: PointA, endPoint: PointA) => void;
-  drawLine: (beginPoint: PointA, endPoint: PointA) => void;
-  drawDashLine: (beginPoint: PointA, endPoint: PointA) => void;
-  getImageData: () => ImageData;
-} & CanvasRenderingContext2D;
+export type CanvasInstance = CanvasExtendImp & CanvasRenderingContext2D;
 
-/**
- * canvas 直接操作的封装
- * @returns CanvasInstance
- */
-export default function useCanvas(): CanvasInstance & CanvasRenderingContext2D {
-  const canvasRef = ref<CanvasRenderingContext2D>();
-  const canvasConfig = ref<CanvasConfig>({
+export class ExtendCanvas implements CanvasExtendImp {
+  private canvasInstance: CanvasRenderingContext2D | null = null;
+  private canvasConfig: CanvasConfig = {
     style: {},
     zoom: 1,
     lineWidth: 1,
     color: 'red',
     density: 1,
     autoConnect: true,
-  });
-  function setupCanvas(canvas: CanvasRenderingContext2D, config?: CanvasConfig) {
-    canvasRef.value = canvas;
-    if (config) {
-      canvasConfig.value = config;
-    }
-    if (historyState.current < 0) {
-      save();
-    }
-  }
-  const getCanvas = () => {
-    const ctx = unref(canvasRef);
-    if (ctx) return ctx;
-    throw new Error('Canvas Not Found');
   };
+  // 橡皮擦用
+  private lastPoint: PointA | null = null;
   // canvas 存储历史用于保存、撤销、还原操作
-  const canvasHistory: ImageData[] & Recordable = [];
-  const historyState = reactive({
+  private canvasHistory: ImageData[] & Recordable = [];
+  private historyState = {
     current: -1,
     max: -1,
-  });
-  canvasHistory.putIn = function (value: ImageData) {
-    if (historyState.max === 19) {
-      this.shift();
-      historyState.max = historyState.current;
-    } else {
-      this.splice(++historyState.current);
-      historyState.max = historyState.current;
-    }
-    this.push(value);
   };
-  const save = () => {
-    const ctx = getCanvas();
+  constructor() {}
+  setupCanvas(canvasInstance: CanvasRenderingContext2D, config?: CanvasConfig) {
+    this.canvasInstance = canvasInstance;
+    if (config) {
+      this.canvasConfig = config;
+    }
+    if (this.historyState.current < 0) {
+      this.save();
+    }
+  }
+  getCanvas() {
+    if (this.canvasInstance) return this.canvasInstance;
+    throw new Error('Canvas Not Found');
+  }
+  save() {
+    const ctx = this.getCanvas();
     const data = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
-    canvasHistory.putIn(data);
-  };
-  const redo = () => {
-    const ctx = getCanvas();
-    if (historyState.current < historyState.max) {
-      const data = canvasHistory[++historyState.current];
+    if (this.historyState.max === 19) {
+      this.canvasHistory.shift();
+      this.historyState.max = this.historyState.current;
+    } else {
+      this.canvasHistory.splice(++this.historyState.current);
+      this.historyState.max = this.historyState.current;
+    }
+    this.canvasHistory.push(data);
+  }
+  redo() {
+    const ctx = this.getCanvas();
+    if (this.historyState.current < this.historyState.max) {
+      const data = this.canvasHistory[++this.historyState.current];
       if (!data) return;
       ctx.putImageData(data, 0, 0);
     }
-  };
-  const undo = () => {
-    const ctx = getCanvas();
-    if (historyState.current > 0) {
-      const data = canvasHistory[--historyState.current];
+  }
+  undo() {
+    const ctx = this.getCanvas();
+    if (this.historyState.current > 0) {
+      const data = this.canvasHistory[--this.historyState.current];
       if (!data) return;
       ctx.putImageData(data, 0, 0);
     }
-  };
+  }
 
   // 清空
-  function clean() {
-    const ctx = getCanvas();
+  clean() {
+    const ctx = this.getCanvas();
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    save();
+    this.save();
   }
   // 橡皮擦
-  let lastPoint: PointA | null = null;
-  const erase = (point: PointA, isLast: boolean = false) => {
-    const ctx = getCanvas();
-    if (!lastPoint) {
-      lastPoint = point;
+  erase = (point: PointA, isLast: boolean = false) => {
+    const ctx = this.getCanvas();
+    if (!this.lastPoint) {
+      this.lastPoint = point;
     } else {
       ctx.save();
       //设置擦除路径
       ctx.beginPath();
       // 清除上一次erase产生的圆和内部内容
-      ctx.arc(lastPoint.x, lastPoint.y, 11, 0, Math.PI * 2, false);
+      ctx.arc(this.lastPoint.x, this.lastPoint.y, 11, 0, Math.PI * 2, false);
       // 通过clip设置下一步清空时，只影响arc的内容
       ctx.clip();
       ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -114,18 +96,18 @@ export default function useCanvas(): CanvasInstance & CanvasRenderingContext2D {
       ctx.clip();
       ctx.stroke();
       ctx.restore();
-      lastPoint = point;
+      this.lastPoint = point;
     } else {
-      lastPoint = null;
+      this.lastPoint = null;
     }
   };
   // 贝塞尔曲线
-  function drawSmoothLine(beginPoint: PointA, controlPoint: PointA, endPoint: PointA) {
-    const ctx = getCanvas();
-    ctx.strokeStyle = canvasConfig.value.color;
+  drawSmoothLine(beginPoint: PointA, controlPoint: PointA, endPoint: PointA) {
+    const ctx = this.getCanvas();
+    ctx.strokeStyle = this.canvasConfig.color;
     ctx.beginPath();
     ctx.moveTo(beginPoint.x, beginPoint.y);
-    ctx.lineWidth = canvasConfig.value.lineWidth;
+    ctx.lineWidth = this.canvasConfig.lineWidth;
     ctx.lineJoin = 'round';
     ctx.lineCap = 'round';
     ctx.quadraticCurveTo(controlPoint.x, controlPoint.y, endPoint.x, endPoint.y);
@@ -133,59 +115,58 @@ export default function useCanvas(): CanvasInstance & CanvasRenderingContext2D {
     ctx.closePath();
   }
   // 普通直线
-  function drawLine(beginPoint: PointA, endPoint: PointA) {
-    const ctx = getCanvas();
-    ctx.strokeStyle = canvasConfig.value.color;
+  drawLine(beginPoint: PointA, endPoint: PointA) {
+    const ctx = this.getCanvas();
+    ctx.strokeStyle = this.canvasConfig.color;
     ctx.beginPath();
-    ctx.lineWidth = canvasConfig.value.lineWidth;
+    ctx.lineWidth = this.canvasConfig.lineWidth;
     ctx.moveTo(beginPoint.x, beginPoint.y);
     ctx.lineTo(endPoint.x, endPoint.y);
     ctx.stroke();
     ctx.closePath();
   }
   // 画虚线
-  function drawDashLine(beginPoint: PointA, endPoint: PointA) {
-    const ctx = getCanvas();
+  drawDashLine(beginPoint: PointA, endPoint: PointA) {
+    const ctx = this.getCanvas();
     ctx.setLineDash([5, 5]);
     ctx.strokeStyle = 'gray';
-    ctx.lineWidth = canvasConfig.value.lineWidth;
+    ctx.lineWidth = this.canvasConfig.lineWidth;
     ctx.beginPath();
     ctx.moveTo(beginPoint.x, beginPoint.y);
     ctx.lineTo(endPoint.x, endPoint.y);
     ctx.stroke();
     ctx.closePath();
   }
-  function getImageData() {
-    const ctx = getCanvas();
+  getImageData() {
+    const ctx = this.getCanvas();
     return ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
   }
+}
 
-  const canvasInstance = {
-    setupCanvas,
-    save,
-    redo,
-    undo,
-    clean,
-    erase,
-    drawSmoothLine,
-    drawLine,
-    drawDashLine,
-    getImageData,
-  };
+/**
+ * canvas 直接操作的封装
+ * @returns CanvasInstance
+ */
+export default function useCanvas(): CanvasInstance {
+  const extendCanvas = new ExtendCanvas();
   // 代理一下
-  return new Proxy(canvasInstance, {
+  return new Proxy(extendCanvas, {
     get: (obj: any, prop) => {
-      if (obj.hasOwnProperty(prop)) {
+      if (Reflect.has(obj, prop)) {
         return obj[prop];
       } else {
-        const ctx = getCanvas() as any;
+        const ctx = obj.getCanvas() as any;
         const value = ctx[prop];
         return isFunction(value) ? value.bind(ctx) : value;
       }
     },
     set: (obj, prop, newValue) => {
-      const ctx = getCanvas() as any;
-      ctx[prop] = newValue;
+      if (Reflect.has(obj, prop)) {
+        obj[prop] = newValue;
+      } else {
+        const ctx = obj.getCanvas() as any;
+        ctx[prop] = newValue;
+      }
       return true;
     },
   }) as CanvasInstance;
