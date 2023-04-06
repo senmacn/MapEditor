eb997a9e15e94728f3575eefd25791894200d409
ChenYijin <chenyijin@pwrd.com>
2023-04-06 15:36:41 +0800

    删除栅格编辑代码

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
-import gridEditor from '../views/grid-editor/grid-editor.vue';
 import defaultEditor from '../views/default-editor/default-editor.vue';
 
 const routes: RouteRecordRaw[] = [
@@ -7,11 +6,6 @@ const routes: RouteRecordRaw[] = [
     name: 'home',
     path: '/',
     redirect: '/default-editor',
-  },
-  {
-    name: 'grid-editor',
-    path: '/grid-editor',
-    component: gridEditor,
   },
   {
     name: 'default-editor',
--- src/views/grid-editor/common/config.ts
+++
@@ -1,3 +0,0 @@
-export const IMAGE_SIZE = 1000;
-
-export const GRID_SIZE = 10;
