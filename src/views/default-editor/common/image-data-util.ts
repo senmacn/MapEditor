bfc8160bbd7934c7e39c4c691503b149437ebad2
senmacn <15652581953@163.com>
2023-04-23 11:21:35 +0800

    修复getClosedCurvePointsData

--- src/views/default-editor/common/image-data-util.ts
+++ src/views/default-editor/common/image-data-util.ts
@@ -172,12 +172,14 @@ export function getImageDataBoundRect(imageData: ImageData): Box {
 export function getClosedCurvePointsData(area: Area) {
   const rect = area.getBoundRect();
   const imageData = area.getData();
+  const newImageData = new ImageData(rect[2], rect[3]);
   const boundPoints = getPosition(imageData);
   for (let yIndex = 0; yIndex < rect[3]; yIndex++) {
     for (let xIndex = 0; xIndex < rect[2]; xIndex++) {
       const pointStartIndex = xIndex * 4 + yIndex * 4 * imageData.width;
       // 是点的话直接加入（加上偏移量）
-      if (isPointInData(imageData.data, pointStartIndex)) {
+      if (boundPoints.some((point) => point[0] === xIndex && point[1] === yIndex)) {
+        newImageData.data[pointStartIndex] = 1;
       } else {
         // 跳过边界上的
         if (xIndex === 0 || xIndex === rect[2] - 1 || yIndex === 0 || yIndex === rect[3] - 1) {
@@ -185,30 +187,93 @@ export function getClosedCurvePointsData(area: Area) {
         } else {
           if (isPointInPolygon([xIndex, yIndex], boundPoints)) {
             // 描点
-            imageData.data[pointStartIndex] = 1;
+            newImageData.data[pointStartIndex] = 1;
           }
         }
       }
     }
   }
-  return imageData;
+  return newImageData;
 }
 
-function isPointInPolygon(point, polygon) {
-  let crossing = 0;
+function isPointInPolygon(point: Point, pts: Point[]) {
+  //下述代码来源：http://paulbourke.net/geometry/insidepoly/，进行了部分修改
+  //基本思想是利用射线法，计算射线与多边形各边的交点，如果是偶数，则点在多边形外，否则
+  //在多边形内。还会考虑一些特殊情况，如点在多边形顶点上，点在多边形边上等特殊情况。
+  var N = pts.length;
+  var boundOrVertex = true; //如果点位于多边形的顶点或边上，也算做点在多边形内，直接返回true
+  var intersectCount = 0; //cross points count of x
+  var precision = 2e-10; //浮点类型计算时候与0比较时候的容差
+  var p1, p2; //neighbour bound vertices
+  var p = point; //测试点
 
-  for (let i = 0, len = polygon.length; i < len; i++) {
-    const p1 = polygon[i];
-    const p2 = polygon[(i + 1) % len];
+  p1 = pts[0]; //left vertex
+  for (var i = 1; i <= N; ++i) {
+    //check all rays
+    if (p[0] === p1[0] && p[1] === p1[1]) {
+      return boundOrVertex; //p is an vertex
+    }
 
-    if (point[1] >= Math.min(p1[1], p2[1]) && point[1] < Math.max(p1[1], p2[1])) {
-      const x = ((point[1] - p1[1]) * (p2[0] - p1[0])) / (p2[1] - p1[1]) + p1[0];
+    p2 = pts[i % N]; //right vertex
+    if (p[0] < Math.min(p1[0], p2[0]) || p[0] > Math.max(p1[0], p2[0])) {
+      //ray is outside of our interests
+      p1 = p2;
+      continue; //next ray left point
+    }
 
-      if (x > point[0]) {
-        crossing++;
+    if (p[0] > Math.min(p1[0], p2[0]) && p[0] < Math.max(p1[0], p2[0])) {
+      //ray is crossing over by the algorithm (common part of)
+      if (p[1] <= Math.max(p1[1], p2[1])) {
+        //x is before of ray
+        if (p1[0] == p2[0] && p[1] >= Math.min(p1[1], p2[1])) {
+          //overlies on a horizontal ray
+          return boundOrVertex;
+        }
+
+        if (p1[1] == p2[1]) {
+          //ray is vertical
+          if (p1[1] == p[1]) {
+            //overlies on a vertical ray
+            return boundOrVertex;
+          } else {
+            //before ray
+            ++intersectCount;
+          }
+        } else {
+          //cross point on the left side
+          var xinters = ((p[0] - p1[0]) * (p2[1] - p1[1])) / (p2[0] - p1[0]) + p1[1]; //cross point of lng
+          if (Math.abs(p[1] - xinters) < precision) {
+            //overlies on a ray
+            return boundOrVertex;
+          }
+
+          if (p[1] < xinters) {
+            //before ray
+            ++intersectCount;
+          }
+        }
+      }
+    } else {
+      //special case when ray is crossing through the vertex
+      if (p[0] == p2[0] && p[1] <= p2[1]) {
+        //p crossing over p2
+        var p3 = pts[(i + 1) % N]; //next vertex
+        if (p[0] >= Math.min(p1[0], p3[0]) && p[0] <= Math.max(p1[0], p3[0])) {
+          //p[0] lies between p1[0] & p3[0]
+          ++intersectCount;
+        } else {
+          intersectCount += 2;
+        }
       }
     }
+    p1 = p2; //next ray left point
   }
 
-  return crossing % 2 == 1;
+  if (intersectCount % 2 == 0) {
+    //偶数在多边形外
+    return false;
+  } else {
+    //奇数在多边形内
+    return true;
+  }
 }
