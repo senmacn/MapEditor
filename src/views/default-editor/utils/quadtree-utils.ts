a4dae5d06349ac32964d42007342af190b33747e
senmacn <15652581953@163.com>
2023-06-01 14:52:13 +0800

    优化地图数据导出

--- src/views/default-editor/utils/quadtree-utils.ts
+++ src/views/default-editor/utils/quadtree-utils.ts
@@ -1,3 +1,4 @@
+import ExtendedDataView from '@/utils/dataview';
 import { getPositionCount, isPointInData } from './image-data-util';
 
 const KB = 1024 * 8;
@@ -37,7 +38,6 @@ export function dataToBin(
   const size = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   let max = 0;
   const needRecord: Recordable<[number, number][]> = {};
-  const blockWidth_CMs: number[] = [];
 
   // 分层计算
   for (let layerIndex = 0; layerIndex <= 15; layerIndex++) {
@@ -47,172 +47,140 @@ export function dataToBin(
     // 该层数据密度
     const grids = 1 << layerIndex;
     // 根据数据密度划分图块
-    // 图块尺寸(对齐到最近的2的次方)
-    const blocks = layerIndex >= 8 ? grids / 64 : grids;
-
+    const blocks = layerIndex >= 8 ? 8 : grids;
+    // 图块尺寸
+    const blockSize = grids / blocks;
     // 图块实际大小
-    const blockWidth_CM = MAX_WIDTH_CM / blocks;
-    blockWidth_CMs.push(blockWidth_CM);
+    const blockWidth_CM = MAX_WIDTH_CM / blockSize;
     // 区块不可能占据某一块
     // 最小128cm
     if (layerWidth_CM < 128) break;
 
-    // 其他层计算
-    let layerData = new ArrayBuffer(grids * grids * 4);
-    let layerDataView = new DataView(layerData);
+    let layerDataView = new ExtendedDataView(layerIndex >= 8 ? (grids * grids) / 16 : 5120);
+    // 用 1byte 的二进制位记录下图块尺寸（先默认等于分层块尺寸）
+    layerDataView.addByte(Math.log2(blocks));
     let dataFlag = false;
-    let _offset = 0;
-    for (let _y = 0; _y < blocks; _y++) {
-      for (let _x = 0; _x < blocks; _x++) {
-        let state;
-        // 判断是否需要记录，不需要就直接跳过了
-        if (layerIndex > 0 && needRecord[layerIndex - 1].length > 1) {
-          let flag = false;
-          const preLayerWidth_CM = blockWidth_CMs[layerIndex - 1];
-          for (let _i = 0; _i < needRecord[layerIndex - 1].length; _i++) {
-            const element = needRecord[layerIndex - 1][_i];
-            if (
-              element[0] * preLayerWidth_CM <= _x * blockWidth_CM &&
-              (element[0] + 1) * preLayerWidth_CM > _x * blockWidth_CM &&
-              element[1] * preLayerWidth_CM <= _y * blockWidth_CM &&
-              (element[1] + 1) * preLayerWidth_CM > _y * blockWidth_CM
-            ) {
-              flag = true;
-              break;
+    let prevData: number | null = null;
+    let repeatFlag = 0;
+    function _computeRepeat(element: number | null) {
+      if (element !== null && element === prevData) {
+        repeatFlag++;
+        return;
+      }
+      if (element !== null && repeatFlag == 0) {
+        repeatFlag++;
+        prevData = element;
+        return;
+      }
+      // <0b11-(0b110-0b{7})*-0b{2}>
+      if (repeatFlag === 1) {
+        layerDataView.addTwoBits(0b11);
+        layerDataView.addTwoBits(prevData);
+      } else {
+        layerDataView.addTwoBits(0b11);
+        // 满 127 一个 0b110-0b{7}1111111 不计当前
+        const repeatFlagCount = Math.floor((repeatFlag - 1) / 127);
+        for (let i = repeatFlagCount; i > 0; i--) {
+          layerDataView.addStr('1101111111');
+        }
+        // 剩余的 不计当前
+        const repeatFlagLast = (repeatFlag - 1) % 127;
+        if (repeatFlagLast >= 1) {
+          layerDataView.addStr('110' + getBits(repeatFlagLast, 7));
+        }
+        layerDataView.addTwoBits(prevData);
+      }
+      if (element != null) {
+        repeatFlag = 1;
+        prevData = element as number;
+      } else {
+        repeatFlag = 0;
+        prevData = null;
+      }
+    }
+    for (let _y = 0; _y < blockSize; _y++) {
+      for (let _x = 0; _x < blockSize; _x++) {
+        // 第0层不需要考虑00
+        if (layerIndex == 0) continue;
+        let _states = '';
+        let _statesNum = { '01': 0, '10': 0, '00': 0 };
+        // 遍历图块中的单元格
+        for (let indexY = 0; indexY < blocks; indexY++) {
+          for (let indexX = 0; indexX < blocks; indexX++) {
+            // 判断是否需要记录，不需要就直接跳过了
+            if (layerIndex > 0 && needRecord[layerIndex - 1].length > 0) {
+              let flag = false;
+              for (let _i = 0; _i < needRecord[layerIndex - 1].length; _i++) {
+                const element = needRecord[layerIndex - 1][_i];
+                if (
+                  element[0] * 2 <= _x * blocks + indexX &&
+                  (element[0] + 1) * 2 > _x * blocks + indexX &&
+                  element[1] * 2 <= _y * blocks + indexY &&
+                  (element[1] + 1) * 2 > _y * blocks + indexY
+                ) {
+                  flag = true;
+                  break;
+                }
+              }
+              if (!flag) {
+                _states += '10';
+                _statesNum[10]++;
+                continue;
+              }
             }
-          }
-          if (!flag) {
-            const num = layerDataView.getUint8(Math.floor(_offset / 8));
-            const formatState = Number('0b' + '10') << (6 - (_offset % 8));
-            layerDataView.setUint8(Math.floor(_offset / 8), num + formatState);
-            _offset = _offset + 2;
-            continue;
-          }
-        }
-        // 加上区域的偏移量和delt [x, y] -> [0, 0]
-        state = executeBlockState(
-          imageData,
-          blockWidth_CM,
-          _x * blockWidth_CM - deltX_CM,
-          _y * blockWidth_CM - deltY_CM,
-        );
-        const num = layerDataView.getUint8(Math.floor(_offset / 8));
-        const formatState = Number('0b' + state) << (6 - (_offset % 8));
-        layerDataView.setUint8(Math.floor(_offset / 8), num + formatState);
-        _offset = _offset + 2;
-        if (state == '01') {
-          dataFlag = true;
-        }
-        if (state == '00' && blockWidth_CM > layerWidth_CM) {
-          dataFlag = true;
-          // 下一层第几块需要记录
-          needRecord[layerIndex].push([_x, _y]);
-          // 遍历图块中的单元格
-          for (let indexY = 0; indexY < blocks; indexY++) {
-            for (let indexX = 0; indexX < blocks; indexX++) {
-              // 加上区域的偏移量和delt [x, y] -> [0, 0]
-              const _state = executeBlockState(
-                imageData,
-                layerWidth_CM,
-                (indexX * layerWidth_CM) / 128 - deltX_CM + (_x * blockWidth_CM) / 128,
-                (indexY * layerWidth_CM) / 128 - deltY_CM + (_y * blockWidth_CM) / 128,
-              );
-              const num = layerDataView.getUint8(Math.floor(_offset / 8));
-              layerDataView.setUint8(
-                Math.floor(_offset / 8),
-                num + (Number('0b' + _state) << (6 - (_offset % 8))),
-              );
-              _offset = _offset + 2;
+            // 加上区域的偏移量和delt [x, y] -> [0, 0]
+            const _state = executeBlockState(
+              imageData,
+              layerWidth_CM,
+              indexX * layerWidth_CM - deltX_CM + _x * blockWidth_CM,
+              indexY * layerWidth_CM - deltY_CM + _y * blockWidth_CM,
+            );
+            if (_state === '00') {
+              // 下一层第几块需要记录
+              needRecord[layerIndex].push([_x * blocks + indexX, _y * blocks + indexY]);
             }
+            _states += _state;
+            _statesNum[_state]++;
           }
         }
-        if (state == '10') {
+        // 取余等于0，状态一致
+        if (_statesNum['00'] === blocks * blocks) {
+          dataFlag = true;
+          _computeRepeat(0b00);
+        } else if (_statesNum['01'] === blocks * blocks) {
+          dataFlag = true;
+          _computeRepeat(0b01);
+        } else if (_statesNum['10'] === blocks * blocks) {
+          _computeRepeat(0b10);
+        } else {
+          dataFlag = true;
+          repeatFlag > 0 && _computeRepeat(null);
+          layerDataView.addStr(_states);
         }
       }
     }
+    repeatFlag > 0 && _computeRepeat(null);
     // 没有一个有数据的，跳过
     if (!dataFlag) {
       offsets[layerIndex] = layerIndex > 0 ? offsets[layerIndex - 1] + size[layerIndex - 1] : 0;
       size[layerIndex] = 0;
-
+      layerIndex >= 1 && delete needRecord[layerIndex - 1];
       continue;
-    }
-    // 用 1byte 的二进制位记录下图块尺寸（先默认等于分层块尺寸）
-    let rawFormatLayerData = getBits(Math.log2(blocks), 8);
-    let repeatFlag = 0;
-    let _index = 0;
-    let prevString = '';
-    function _computeRepeat() {
-      const repeatFlagLast = repeatFlag % 128;
-      const repeatFlagCount = Math.floor(repeatFlag / 128);
-      rawFormatLayerData = rawFormatLayerData + '11';
-      for (let i = repeatFlagCount; i > 0; i--) {
-        rawFormatLayerData = rawFormatLayerData + '1101111111';
-      }
-      if (repeatFlagLast > 1) {
-        rawFormatLayerData = rawFormatLayerData + '110' + getBits(repeatFlagLast - 1, 7);
-      }
-      rawFormatLayerData = rawFormatLayerData + prevString;
-      repeatFlag = 0;
-    }
-    while (_index < _offset) {
-      const element = padZero(layerDataView.getUint8(Math.floor(_index / 8)).toString(2), 8).slice(
-        _index % 8,
-        (_index % 8) + 2,
-      );
-      // 单元格数据全部记录
-      if (element == '00') {
-        // 先计算一下（假如不是一上来就是00）
-        prevString.length > 0 && prevString != '00' && _computeRepeat();
-        // 跳过1个图块数据 记录 blocks ^2个单元格数据
-        for (let i = 2; i < blocks * blocks * 2 + 2; i += 2) {
-          const unitNum = padZero(layerDataView.getUint8(Math.floor(i / 8)).toString(2), 8);
-          rawFormatLayerData =
-            rawFormatLayerData + unitNum.slice((_index + i) % 8, ((_index + i) % 8) + 2);
-        }
-        _index = _index + 2 + 2 * blocks * blocks;
-        prevString = '';
-        repeatFlag = 0;
-      } else {
-        // 重复
-        if (element === prevString) {
-          // 最后一次直接计算重复
-          if (_index === _offset - 2) {
-            _computeRepeat();
-          }
-        } else {
-          // 当前一次计算不为全计算时
-          if (prevString != '' && prevString != '00') {
-            // 不重复了计算（包括）上个状态之前的重复
-            _computeRepeat();
-          }
-          // 最后一次直接加上未重复
-          if (_index === _offset - 2) {
-            rawFormatLayerData = rawFormatLayerData + '11' + element;
-          }
-        }
-        _index = _index + 2;
-        prevString = element;
-        repeatFlag++;
-      }
     }
 
     // 没到 1024 (1KB)则补全
-    while (rawFormatLayerData.length % KB !== 0) {
-      rawFormatLayerData = rawFormatLayerData + '1';
+    while (layerDataView.getBitsLength() % KB !== 0) {
+      layerDataView.addBit(1);
     }
     // 计算偏移字节数
     offsets[layerIndex] = offsets[layerIndex - 1] + size[layerIndex - 1];
-    const offsetBytes = layerIndex > 0 ? offsets[layerIndex] * 1024 : 0;
-    for (let index = 0; index < rawFormatLayerData.length / 32; index++) {
-      view.setUint32(
-        32 + offsetBytes + index * 4,
-        Number('0b' + rawFormatLayerData.slice(index * 32, index * 32 + 32)),
-      );
+    const offsetBytes = offsets[layerIndex] * 1024;
+    for (let index = 0; index < layerDataView.getDataLength(); index++) {
+      view.setUint8(32 + offsetBytes + index, layerDataView.readByte(index));
     }
-    size[layerIndex] = rawFormatLayerData.length / KB;
+    size[layerIndex] = layerDataView.getDataLength() / 1024;
     // 字节数
-    max = BASE_BYTES + offsets[layerIndex] * 1024 + rawFormatLayerData.length / 8 + 1;
+    max = BASE_BYTES + offsets[layerIndex] * 1024 + layerDataView.getDataLength() + 1;
     layerIndex > 1 && delete needRecord[layerIndex - 1];
   }
   offsets.forEach((offset, index) => {
@@ -286,8 +254,4 @@ function executeBlockState(imageData, blockWidth_cm, x_cm, y_cm) {
       return '00';
     }
   }
-}
-
-function nearestPowerOfTwo(n) {
-  return Math.floor(Math.log2(n)) === Math.log2(n) ? n / 2 : Math.pow(2, Math.floor(Math.log2(n)));
 }
