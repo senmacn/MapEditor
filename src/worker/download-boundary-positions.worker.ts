3ac31753dc278021008fbe3f7b55066dda16eaa2
ChenYijin <chenyijin@pwrd.com>
2023-07-24 20:03:39 +0800

    添加electron-store用于管理本地配置；添加基本自定义设置；导出边框数据使用中心点的坐标；

--- src/worker/download-boundary-positions.worker.ts
+++ src/worker/download-boundary-positions.worker.ts
@@ -6,9 +6,10 @@ addEventListener(
     const data = event.data;
     const [imageData, offsetX, offsetY, ltX, ltY, scale, type] = data;
     const points = getPosition(imageData);
+    // 取转换厘米后方块中心点
     const formatPoints = points.map((point) => [
-      ltX + (point[0] + offsetX) * scale,
-      ltY + (point[1] + offsetY) * scale,
+      ltX + (point[0] + offsetX) * scale + 50,
+      ltY + (point[1] + offsetY) * scale + 50,
     ]);
     postMessage({
       type: type,
