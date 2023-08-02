da20318654ec234d2a72382914836e323971ad98
senmacn <15652581953@163.com>
2023-08-02 11:54:37 +0800

    优化标尺显示，修复边框数据导出计算

--- src/worker/download-boundary-positions.worker.ts
+++ src/worker/download-boundary-positions.worker.ts
@@ -4,16 +4,14 @@ addEventListener(
   'message',
   function (event) {
     const data = event.data;
-    const [imageData, ltX, ltY, scale, type] = data;
+    const [imageData, offsetX, offsetY, ltX, ltY, scale, type] = data;
     const points = getPosition(imageData);
     // 取转换厘米后方块中心点
     const formatPoints = points.map((point) => [
-      ltX + point[0] * scale + 50,
-      ltY + point[1] * scale + 50,
+      ltX + (point[0] + offsetX) * scale + 50,
+      ltY + (point[1] + offsetY) * scale + 50,
     ]);
-    console.log('ltX', ltX);
-    console.log('scale', scale);
-    postMessage({
+    postMessage({  
       type: type,
       value: formatPoints,
     });
